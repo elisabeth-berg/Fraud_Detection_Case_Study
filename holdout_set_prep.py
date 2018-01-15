dcaef7b57111699c45f5266e10a77362cac8c704
Christiana Smith <cj.smith015@gmail.com>
2018-01-15 15:03:58 -0800

    Add reset index in holdout_set_prep.py

--- holdout_set_prep.py
+++ holdout_set_prep.py
@@ -9,5 +9,5 @@ full_data = pd.read_json('data/data.json')
 
 train_data, holdout_data = train_test_split(full_data, test_size=0.20, random_state=56)
 
-train_data.to_json('data/train_data.json')
-holdout_data.to_json('data/holdout_data.json')
+train_data.reset_index(drop=True).to_json('data/train_data.json')
+holdout_data.reset_index(drop=True).to_json('data/holdout_data.json')
